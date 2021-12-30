857dac19197832e9d75640327570160a6364638d
hiroto.ohira <hiroto100114@gmail.com>
2021-12-30 12:17:25 +0900

    Display TextField.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,13 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import TextField from "@mui/material/TextField";
 
 function App() {
-    return (
-        <div className="App">
-            foo!!!
-        </div>
-    );
+  return (
+    <div className="App">
+      <TextField variant="standard" multiline />
+    </div>
+  );
 }
 
 export default App;
