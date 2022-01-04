cc7c3908859a90e9f6c7fb1af1ca7e6aef20f2a6
hiroto.ohira <hiroto100114@gmail.com>
2022-01-04 20:47:58 +0900

    linaria の Interpolations に型情報を付与
    - Mindmap コンポーネントの絶対座標配置ができていない

--- frontend/src/components/pages/Mindmap.tsx
+++ frontend/src/components/pages/Mindmap.tsx
@@ -1,12 +1,31 @@
-import React from "react";
+import React, { FC } from "react";
+import { styled } from "@linaria/react";
 import RootNode from "../organisms/RootNode";
 
-function Mindmap() {
+type PositionWrapperProps = {
+  windowWidth: number;
+  windowHeight: number;
+};
+
+const PositionWrapper = styled.div<PositionWrapperProps>`
+  position: absolute;
+  top: ${(props) => props.windowHeight / 2};
+  left: ${(props) => props.windowWidth / 2};
+`;
+
+const Mindmap: FC = () => {
+  const windowWidth: number = window.innerWidth;
+  const windowHeight: number = window.innerHeight;
+
   return (
-    <div>
+    <PositionWrapper
+      id="PositionWrapper"
+      windowWidth={windowWidth}
+      windowHeight={windowHeight}
+    >
       <RootNode />
-    </div>
+    </PositionWrapper>
   );
-}
+};
 
 export default Mindmap;
