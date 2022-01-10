514dda5573701de112f8028042d0eba0e8a79165
hiroto.ohira <hiroto100114@gmail.com>
2022-01-10 14:20:54 +0900

    Rewrite useing useState.

--- frontend/src/components/pages/Mindmap.tsx
+++ frontend/src/components/pages/Mindmap.tsx
@@ -1,21 +1,21 @@
-import React, { FC } from "react";
+import React, { FC, useState } from "react";
 import { styled } from "@linaria/react";
 import RootNode from "../organisms/RootNode";
 
-type PositionWrapperProps = {
+type PositionWrapperStyleProps = {
   windowWidth: number;
   windowHeight: number;
 };
 
-const PositionWrapper = styled.div<PositionWrapperProps>`
+const PositionWrapper = styled.div<PositionWrapperStyleProps>`
   position: absolute;
   top: ${(props) => props.windowHeight / 2}px;
   left: ${(props) => props.windowWidth / 2}px;
 `;
 
 const Mindmap: FC = () => {
-  const windowWidth: number = window.innerWidth;
-  const windowHeight: number = window.innerHeight;
+  const [windowWidth] = useState<number>(window.innerWidth);
+  const [windowHeight] = useState<number>(window.innerHeight);
 
   return (
     <PositionWrapper
