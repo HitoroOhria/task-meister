d0648bfaf0dacbbc04edd31bfb808205e9514383
hiroto.ohira <hiroto100114@gmail.com>
2022-01-13 20:17:03 +0900

    Add NodeData

---
+++ frontend/src/domain/model/NodeData.ts
@@ -0,0 +1,13 @@
+class NodeData {
+  private readonly id: string;
+  private width: number;
+  private height: number;
+
+  constructor(id: string) {
+    this.id = id;
+    this.width = 0;
+    this.height = 0;
+  }
+}
+
+export default NodeData;
