6b35285d86513faa73a56c67c9bdb4e7e62f6155
hiroto.ohira <hiroto100114@gmail.com>
2022-02-01 19:23:17 +0900

    Add total func.

--- frontend/src/util/NumberUtil.ts
+++ frontend/src/util/NumberUtil.ts
@@ -1,3 +1,7 @@
+export const total = (list: number[]): number => {
+  return list.reduce((preNumber, curNumber) => sum(preNumber, curNumber), 0);
+};
+
 export const sum = (first: number, second: number): number => {
   return first + second;
 };
