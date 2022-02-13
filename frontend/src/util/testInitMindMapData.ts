dd8537805879009893a2150a83b647fea43dc25c
hiroto.ohira <hiroto100114@gmail.com>
2022-02-13 18:37:04 +0900

    Delete some initMindMapData

--- frontend/src/util/testInitMindMapData.ts
+++ frontend/src/util/testInitMindMapData.ts
@@ -1,29 +1,24 @@
-import { newNode } from "~/domain/model/Node";
-import { newChildren } from "~/domain/model/Children";
-import { newMindMapData } from "~/domain/model/MindMapData";
-import { newRootNode } from "~/domain/model/RootNode";
-import { newRightMap } from "~/domain/model/RightMap";
-import { newGroup } from "~/domain/model/Group";
+import {newNode} from "~/domain/model/Node";
+import {newChildren} from "~/domain/model/Children";
+import {newMindMapData} from "~/domain/model/MindMapData";
+import {newRootNode} from "~/domain/model/RootNode";
+import {newRightMap} from "~/domain/model/RightMap";
+import {newGroup} from "~/domain/model/Group";
 
-const node1_2_1 = newNode("F", "F", newGroup(), newChildren([]));
-const node1_2_2 = newNode("G", "G", newGroup(), newChildren([]));
-const node1_2_3 = newNode("H", "H", newGroup(), newChildren([]));
+// const node1_2_1 = newNode("F", "F", newGroup(), newChildren([]));
+// const node1_2_2 = newNode("G", "G", newGroup(), newChildren([]));
+// const node1_2_3 = newNode("H", "H", newGroup(), newChildren([]));
 
-const node1_1 = newNode("C", "C", newGroup(), newChildren([]));
-const node1_2 = newNode(
-  "D",
-  "D",
-  newGroup(),
-  newChildren([node1_2_1, node1_2_2, node1_2_3])
-);
-const node1_3 = newNode("E", "E", newGroup(), newChildren([]));
+// const node1_1 = newNode("C", "C", newGroup(), newChildren([]));
+// const node1_2 = newNode(
+//   "D",
+//   "D",
+//   newGroup(),
+//   newChildren([node1_2_1, node1_2_2, node1_2_3])
+// );
+// const node1_3 = newNode("E", "E", newGroup(), newChildren([]));
 
-const node1 = newNode(
-  "A",
-  "A",
-  newGroup(),
-  newChildren([node1_1, node1_2, node1_3])
-);
+const node1 = newNode("A", "A", newGroup(), newChildren([]));
 const node2 = newNode("B", "B", newGroup(), newChildren([]));
 
 const initMindMapData = newMindMapData(
