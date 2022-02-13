baced302e1a8349f0c8f966f420d9ee41a96ae26
hiroto.ohira <hiroto100114@gmail.com>
2022-02-13 13:59:18 +0900

    Refactor to use KeydownManager.

---
+++ frontend/src/components/organisms/KeydownManager.tsx
@@ -0,0 +1,38 @@
+import React, { useContext, useEffect, VFC } from "react";
+import { mindMapDataActionType as actionType } from "~/store/reducer/MindMapDataReducer";
+import {
+  MindMapDispatchCtx,
+  MindMapStateCtx,
+} from "~/store/context/MindMapDataCtx";
+import { getShortcut } from "~/enum/Shortcut";
+
+const KeydownManager: VFC = () => {
+  const mindMapData = useContext(MindMapStateCtx);
+  const dispatchMindMapData = useContext(MindMapDispatchCtx);
+
+  const handleKeydown = (e: KeyboardEvent) => {
+    if (mindMapData.isInputting) return;
+
+    const shortcut = getShortcut(e.key);
+    if (!shortcut) return;
+    const selectedNodeId =
+      mindMapData.rightMap.children.recursively.findNodeIsSelected()?.id;
+    if (!selectedNodeId) return;
+
+    dispatchMindMapData({
+      type: actionType.processKeydown,
+      payload: { shortcut, selectedNodeId },
+    });
+  };
+
+  const keydownEffect = (): (() => void) => {
+    document.body.addEventListener("keydown", handleKeydown);
+    return () => document.body.removeEventListener("keydown", handleKeydown);
+  };
+
+  useEffect(keydownEffect, [handleKeydown]);
+
+  return <></>;
+};
+
+export default KeydownManager;
--- frontend/src/components/pages/MindMap.tsx
+++ frontend/src/components/pages/MindMap.tsx
@@ -1,50 +1,26 @@
-import React, { useContext, useEffect, VFC } from "react";
+import React, { useContext, VFC } from "react";
 import DroppableArea from "~/components/organisms/DroppableArea";
 import Origin from "~/components/organisms/Origin";
 import Node from "~/components/organisms/Node";
 import Nodes from "~/components/organisms/Nodes";
-import { mindMapDataActionType as actionType } from "~/store/reducer/MindMapDataReducer";
-import {
-  MindMapDispatchCtx,
-  MindMapStateCtx,
-} from "~/store/context/MindMapDataCtx";
-import { getShortcut } from "~/enum/Shortcut";
+import { MindMapStateCtx } from "~/store/context/MindMapDataCtx";
+import KeydownManager from "~/components/organisms/KeydownManager";
 
 const MindMap: VFC = () => {
   const mindMapData = useContext(MindMapStateCtx);
-  const dispatchMindMapData = useContext(MindMapDispatchCtx);
-
-  const handleKeydown = (e: KeyboardEvent) => {
-    if (mindMapData.isInputting) return;
-
-    const shortcut = getShortcut(e.key);
-    if (!shortcut) return;
-    const selectedNodeId =
-      mindMapData.rightMap.children.recursively.findNodeIsSelected()?.id;
-    if (!selectedNodeId) return;
-
-    dispatchMindMapData({
-      type: actionType.processKeydown,
-      payload: { shortcut, selectedNodeId },
-    });
-  };
-
-  const handleKeydownEventListerEffect = (): (() => void) => {
-    document.body.addEventListener("keydown", handleKeydown);
-    return () => document.body.removeEventListener("keydown", handleKeydown);
-  };
-
-  useEffect(handleKeydownEventListerEffect, [handleKeydown]);
 
   // TODO Why is display smaller on monitor?
   return (
-    <DroppableArea>
-      <Origin>
-        {/* TODO Make tail of root node to draggable */}
-        <Node nodeData={mindMapData.rootNode} />
-        <Nodes nodes={mindMapData.rightMap.children} />
-      </Origin>
-    </DroppableArea>
+    <>
+      <KeydownManager />
+      <DroppableArea>
+        <Origin>
+          {/* TODO Make tail of root node to draggable */}
+          <Node nodeData={mindMapData.rootNode} />
+          <Nodes nodes={mindMapData.rightMap.children} />
+        </Origin>
+      </DroppableArea>
+    </>
   );
 };
 
