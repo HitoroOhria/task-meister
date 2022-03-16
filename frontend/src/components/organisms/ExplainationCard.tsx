d31fd902c2ce54ee156bf7d1b02efc6b39203d71
hiroto.ohira <hiroto100114@gmail.com>
2022-03-16 20:14:37 +0900

    Add ShortcutRow to ExplanationCard.

--- frontend/src/components/organisms/ExplainationCard.tsx
+++ frontend/src/components/organisms/ExplainationCard.tsx
@@ -1,7 +1,7 @@
 import React, { VFC } from 'react'
 
 import { styled } from '@linaria/react'
-import { Card } from '@nextui-org/react'
+import { Card, Text } from '@nextui-org/react'
 import { MdKeyboardTab } from 'react-icons/md'
 
 // Margin from window. Unit is px.
@@ -13,9 +13,12 @@ const ExplanationCard: VFC<Props> = (props) => {
   return (
     <PositionFixed>
       <Card>
-        <Card shadow={false} css={{ backgroundColor: '#E4E4E4' }}>
-          <MdKeyboardTab />
-        </Card>
+        <ShortcutRow>
+          <Card shadow={false} css={{ backgroundColor: '#E4E4E4' }}>
+            <MdKeyboardTab />
+          </Card>
+          <Text>要素を追加</Text>
+        </ShortcutRow>
       </Card>
     </PositionFixed>
   )
@@ -27,4 +30,8 @@ const PositionFixed = styled.div`
   position: fixed
   top: ${margin}px
   right: ${margin}px
+`
+
+const ShortcutRow = styled.div`
+  display: flex;
 `
