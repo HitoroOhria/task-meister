26070c17454e91f1409a96669488f08d32ba4c5f
hiroto.ohira <hiroto100114@gmail.com>
2022-03-16 20:00:30 +0900

    Add KeyboardTab icon to ExplainCard.

--- frontend/src/components/organisms/ExplainCard.tsx
+++ frontend/src/components/organisms/ExplainCard.tsx
@@ -2,6 +2,7 @@ import React, { VFC } from 'react'
 
 import { styled } from '@linaria/react'
 import { Card } from '@nextui-org/react'
+import { MdKeyboardTab } from 'react-icons/md'
 
 // Margin from window. Unit is px.
 const margin = 20
@@ -11,7 +12,11 @@ type Props = {}
 const ExplanationCard: VFC<Props> = (props) => {
   return (
     <PositionFixed>
-      <Card>Explain Card</Card>
+      <Card>
+        <Card shadow={false} css={{ backgroundColor: '#E4E4E4' }}>
+          <MdKeyboardTab />
+        </Card>
+      </Card>
     </PositionFixed>
   )
 }
